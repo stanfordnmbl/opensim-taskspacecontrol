ef82c32be3eae376e30155be6a4b26fe63c924c9
Christopher Dembia <cld72@cornell.edu>
2013-10-18 18:45:29 -0700

    Add first source file.

---
+++ TaskSpace/TaskSpaceController.h
@@ -0,0 +1,17 @@
+#ifndef OPENSIM_TASKSPACE_CONTROLLER_H_
+#define OPENSIM_TASKSPACE_CONTROLLER_H_
+
+namespace OpenSim {
+
+namespace TaskSpace {
+
+class Controller : public OpenSim::Controller
+{
+
+};
+
+} // namespace TaskSpace
+
+} // namespace OpenSim
+
+#endif
