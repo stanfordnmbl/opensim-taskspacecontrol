3a07f6f95f777dbfc16643ea184b944fb1135057
Christopher Dembia <cld72@cornell.edu>
2014-04-20 19:33:40 -0700

    Doxygen and sketch implementations for PriorityLevel.

--- TaskSpace/PriorityLevel.cpp
+++ TaskSpace/PriorityLevel.cpp
@@ -6,19 +6,115 @@ using SimTK::Vector;
 
 using OpenSim::TaskSpace;
 
-Matrix TaskSpace::PriorityLevel::jacobian(const State& s)
-
-Matrix TaskSpace::PriorityLevel::nullspaceProjectionTranspose(const State& s)
+PriorityLevel::PriorityLevel()
 {
-    // TODO maybe the tasks don't need to provide a nullspace projection transpose; we just concatenate all the tasks' jacobians and compute a total nullspace projection. i don't think we can just obtain it via concatenation.
 }
 
-Vector TaskSpace::PriorityLevel::generalizedForce(const State& s)
+Vector TaskSpace::PriorityLevel::generalizedForces(const State& s)
 {
-    Vector taskSpaceForce(_numDims);
-    for (unsigned int iT = 0; iT < _numTasks; iT++)
+    Vector levelGenForces(getNumScalarTasks());
+
+    // Used to write constituent generalized force vectors to the correct
+    // place.
+    unsigned int STidx = 0;
+
+    for (unsigned int iT = 0; iT < m_taskSet.getSize(); iT++)
     {
-        taskSpaceForce[TODO] = _tasks[iT].taskSpaceForce(s);
+        unsigned int nST = m_taskSet().get(iT).getNumScalarTasks();
+
+        // Write the nST x 1 matrix (vector) to the (STidx, 0) location.
+        levelGenForces.updBlock(STidx, 0, nST, 1) =
+            m_taskSet.get(iT).generalizedForces(s);
+
+
+        STidx += nST;
     }
-    return jacobianTranspose(s) * taskSpaceForce;
+    return generalizedForces;
 }
+
+Matrix TaskSpace::PriorityLevel::nullspaceProjection(const State& s)
+{
+    // Build identity matrix.
+    Matrix identity(s.getNU(), s.getNU());
+    identity.setToZero();
+    identity.diag().setTo(1.0);
+
+    return identity - dynamicallyConsistentJacobianInverse(s) * jacobian(s);
+}
+
+Matrix TaskSpace::PriorityLevel::jacobian(const State&s s)
+{
+    Matrix levelJacobian(getNumScalarTasks(), s.getNU());
+
+    // Used to write constituent jacobians to the correct place.
+    unsigned int STidx = 0;
+
+    for (unsigned int iT = 0; iT < m_taskSet.getSize(); iT++)
+    {
+        unsigned int nST = m_taskSet().get(iT).getNumScalarTasks();
+
+        // Write the nST x NU matrix to the (STidx, 0) location.
+        levelJacobian.updBlock(STidx, 0, nST, s.getNU()) =
+            m_taskSet().get(iT).jacobian(s);
+
+        STidx += nST;
+    }
+    return levelJacobian;
+}
+
+Matrix TaskSpace::PriorityLevel::dynamicallyConsistentJacobianInverse(
+        const State& s)
+{
+    // J^T \Lambda
+    // -----------
+    Matrix jacobianTransposeTimesLambda =
+        jacobian(s).transpose() * taskSpaceMassMatrix(s);
+
+    // A^{-1} J^T \Lambda
+    // ------------------
+    Matrix dynConsistentJacobianInverse(s.getNU(), getNumScalarTasks());
+
+    for (unsigned int iST = 0; iST < m_taskSet.getSize(); iST++)
+    {
+        m_smss.multiplyByMInv(s,
+                jacobianTransposeTimesLambda.col(iST),
+                dynConsistentJacobianInverse.updCol(iST));
+    }
+
+    return dynConsistentJacobianInverse;
+}
+
+Matrix TaskSpace::PriorityLevel::taskSpaceMassMatrix(const State& s)
+{
+    // A^{-1} J^T
+    // -------------
+    Matrix jacobian = jacobian(s);
+    Matrix jacobianTranspose = jacobian.transpose();
+    Matrix systemMassMatrixInverseTimesJacobianTranspose(
+            s.getNU(), getNumScalarTasks());
+
+    for (unsigned int iST = 0; iST < getNumScalarTasks(); iST++)
+    {
+        m_smss.multiplyByMInv(s, jacobianTranspose.col(iST),
+            systemMassMatrixInverseTimesJacobianTranspose.updCol(iST));
+    }
+
+    // J A^{-1} J^T
+    // -------------
+    Matrix taskMassMatrixInverse =
+        jacobian * systemMassMatrixInverseTimesJacobianTranspose;
+
+    // (J A^{-1} J^T)^{-1}
+    // -------------------
+    // TODO compute inverse in a different way; this feels messy.
+    FactorLU taskMassMatrixInverseLU(taskMassMatrixInverse);
+    Matrix taskMassMatrix(s.getNumScalarTasks(), s.getNumScalarTasks());
+    taskMassMatrixInverseLU.inverse(taskMassMatrix);
+
+    return taskMassMatrix;
+}
+
+
+
+
+
--- TaskSpace/PriorityLevel.h
+++ TaskSpace/PriorityLevel.h
@@ -10,6 +10,22 @@ namespace OpenSim {
 namespace TaskSpace {
 
 /**
+ * A PriorityLevel, identified by its index \f$ p \f$, must be able to compute
+ * two quantities (\f$ n \f$ is the number of coordinates in the model):
+ * 1. generalized forces
+ * 2. a nullspace projection matrix
+ *
+ * The nullspace projection matrix depends on:
+ * - this PriorityLevel's jacobian, obtained from its constituent Task's.
+ * - the system's mass matrix, computed by Simbody.
+ * - this PriorityLevel's task-space mass matrix.
+ *
+ * The task-space mass matrix depends on:
+ * - the system's mass matrix
+ * - this PriorityLevel's jacobian
+ *
+ * See the corresponding methods for more information.
+ *
  */
 class OSIMTASKSPACE_API PriorityLevel : public OpenSim::Object
 {
@@ -21,9 +37,120 @@ public:
             "All the tasks in this priority level.");
     /**@}**/
 
+    PriorityLevel();
+
 public:
-    Matrix nullspaceProjectionTranspose(const State& s);
-    Vector generalizedForce(const State& s);
+
+    /**
+     * @brief This quantity is denoted as \f$ \Gamma_p \in \mathbf{R}^n \f$.
+     * These generalized forces attempt to achieve all Task's within the
+     * PriorityLevel.
+     *
+     * This Vector is obtained by concatenating the generalized forces of the
+     * constituent Task:
+     *
+     * \f[
+     * \Gamma_p =
+     * \begin{bmatrix}
+     *       \Gamma_{p1} \\
+     *       \Gamma_{p2} \\
+     *       \vdots \\
+     *       \Gamma_{pT}
+     * \end{bmatrix}
+     * \f]
+     *
+     * where \f$ T \f$ is the number of Task's in this PriorityLevel, and
+     * \f$ \Gamma_{pt} \f$ is the generalized forces for the \f$t\f$-th Task in
+     * PriorityLevel \f$p\f$.
+     */
+    Vector generalizedForces(const State& s);
+
+    /**
+     * @brief This quantity is denoted as \f$ N_p \in \mathbf{R}^{n \times n}
+     * \f$, and is used to ensure tasks of priority lower than this one do not
+     * interfere with tasks of this PriorityLevel.
+     *
+     * This Matrix is computed via:
+     *
+     * \f[
+     * N_p = I - \bar{J}_p J_p
+     * \f]
+     *
+     * where:
+     * - \f$ I \in \mathbf{R}^{n \times n} \f$ is the identity matrix.
+     * - \f$ \bar{J}_p \f$ is this PriorityLevel's dynamically-consistent
+     *   jacobian inverse (see related method).
+     * - \f$ J_p \f$ is this PriorityLevel's jacobian (see related method).
+     *
+     */
+    Matrix nullspaceProjection(const State& s);
+
+    /**
+     * @brief This quantity is denoted as \f$ J_p \in \mathbf{R}^{s \times
+     * n}\f$, where \f$s\f$ is the number of scalar tasks in this priority
+     * level, and \f$n\f$ is the number of degrees of freedom of the Model.
+     *
+     * This matrix is most often used to convert generalized speeds into
+     * Cartesian (task-space) velocities, or Cartesian (task-space) forces into
+     * generalized forces.
+     *
+     * This Matrix is obtained by concatenating the jacobians of the
+     * constituent Task's:
+     *
+     * \f[
+     * J_p =
+     * \begin{bmatrix}
+     *      J_{p1} \\
+     *      J_{p2} \\
+     *      \vdots \\
+     *      J_{pT}
+     * \end{bmatrix}
+     *
+     * \f]
+     */
+    Matrix jacobian(const State&s s);
+
+    /**
+     * @brief This quantity is denoted as \f$ \bar{J}_p \in \mathbf{R}^{n
+     * \times s} \f$, where \f$n\f$ is the number of degrees of freedom of the
+     * Model, and \f$s\f$ is the number of scalar tasks in this PriorityLevel.
+     *
+     * This Matrix is computed via:
+     *
+     * \f[
+     * \bar{J}_p = A^{-1} J_p^T \Lambda_p
+     * \f]
+     *
+     * where:
+     * - \f$ A \in \mathbf{R}^{n \times n} \f$ is the system's mass matrix (in
+     *   generalized coordinates).
+     * - \f$ J_p \f$ is this PriorityLevel's jacobian (see related method).
+     * - \f$ \Lambda_p \f$ is this PriorityLevel's task-space mass matrix (see
+     *   related method).
+     *
+     */
+    Matrix dynamicallyConsistentJacobianInverse(const State& s);
+
+    /**
+     * @brief This quantity is denoted as \f$ \Lambda_p \in \mathbf{R}^{s
+     * \times s} \f$, where \f$s\f$ is the number of scalar tasks in this
+     * PriorityLevel.
+     *
+     * Ths Matrix is computed via:
+     *
+     * \f[
+     * \Lambda_p = (J_p A^{-1} J_p^T)^{-1}
+     * \f]
+     *
+     * where:
+     * - \f$ A \in \mathbf{R}^{n \times n} \f$ is the system's mass matrix (in
+     *   generalized coordinates).
+     * - \f$ J_p \f$ is this PriorityLevel's jacobian (see related method).
+     */
+    Matrix taskSpaceMassMatrix(const State& s);
+
+
+};
 
 } // namespace TaskSpace
 
